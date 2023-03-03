3101d16f1e2044325d8a4dde1205708396e5ea8b
dw5 <29355584+dw5@users.noreply.github.com>
2023-03-03 19:43:42 +0200

    test

---
+++ VanilloProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -0,0 +1,169 @@
+package com.lagradost
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbUrl
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+
+/* WIP
+search https://api.vanillo.tv/v1/search?query=***
+wont https://api.vanillo.tv/v1/playlists/FTWFYu3JS1qpyXEGtdtQ0A/videos
+as series ? https://api.vanillo.tv/v1/profiles/***/videos?offset=0&limit=10
+no need -- video info https://api.vanillo.tv/v1/videos/***
+POST "{\"videoId\":\"$keyVideoID\"}" https://api.vanillo.tv/v1/_/watch
+to GET playback links https://api.vanillo.tv/v1/_/watch/manifests?watchToken=$watchToken"
+mainpage https://api.vanillo.tv/v1/videos/recommended?limit=30
+*/
+
+
+class VanilloProvider : MainAPI() {
+    override var name = "Vanillo"
+    override var mainUrl = "https://api.vanillo.tv"
+    override val instantLinkLoading = true
+    override val hasQuickSearch = true
+    override val hasChromecastSupport = true // MKV FILES CANT BE PLAYED ON A CHROMECAST
+    override val hasDownloadSupport = false // bc HLS and DASH?
+	override val hasMainPage = false // for now, has recommended though
+
+data class VanSearchResponse(val results: List<VanilloSearchItem>)
+data class VanilloSearchResult(
+@JsonProperty("type") val type: String, // profile or video
+@JsonProperty("id") val id: String, // user or videoID
+@JsonProperty("title") val title: String?, //video only
+@JsonProperty("thumbnail") val thumbnail: String?, //video only
+@JsonProperty("description") val description: String?, //video only
+@JsonProperty("duration") val duration: Double?,
+@JsonProperty("category") val category: String?,
+@JsonProperty("privacy") val privacy: String?,
+//@JsonProperty("defaultThumbnails") val defaultThumbnails: List<String>?,
+@JsonProperty("status") val status: String?,
+@JsonProperty("publishedAt") val publishedAt: String?,
+@JsonProperty("uploader") val uploader: VanilloUploader?
+    )
+
+data class HomePageList(
+@JsonProperty("type") val type: String, // profile or video
+@JsonProperty("id") val id: String, // user or videoID
+@JsonProperty("title") val title: String?, //video only
+@JsonProperty("thumbnail") val thumbnail: String?, //video only
+@JsonProperty("description") val description: String?, //video only
+@JsonProperty("duration") val duration: Double?,
+@JsonProperty("category") val category: String?,
+@JsonProperty("privacy") val privacy: String?,
+//@JsonProperty("defaultThumbnails") val defaultThumbnails: List<String>?,
+@JsonProperty("status") val status: String?,
+@JsonProperty("publishedAt") val publishedAt: String?,
+@JsonProperty("uploader") val uploader: VanilloUploader?
+    )
+	
+data class WatchTokenData(
+@JsonProperty("watchToken") val watchToken: String
+)
+
+data class WatchFinalPlaybackMedia(
+@JsonProperty("dash") val dash: String,
+@JsonProperty("hls") val hls: String
+)
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> {
+        return vanisearch(query)
+    }
+
+    override suspend fun vanisearch(query: String): List<SearchResponse> {
+        val url = "$mainUrl/v1/search?query=$query"
+        val returnValue: ArrayList<SearchResponse> = ArrayList()
+        val response = app.get(url).text
+        val mapped = response.let { mapper.readValue<List<VanilloSearchResult>>(it) }
+        if (mapped.isEmpty()) return returnValue
+
+        for (i in mapped) {
+            val currentUrl = "$mainUrl/v1/_/${i.id}"
+            val currentPoster = "${i.thumbnail}"
+            if (i.type == "profile") { // TV-SERIES
+                returnValue.add(
+                    TvSeriesSearchResponse(
+                        i.title,
+                        currentUrl,
+                        this.name,
+                        TvType.TvSeries,
+                        currentPoster,
+                        i.year,
+                        null
+                    )
+                )
+            } else if (i.type == "video") { // MOVIE
+                returnValue.add(
+                    MovieSearchResponse(
+                        i.title,
+                        currentUrl,
+                        this.name,
+                        TvType.Movie,
+                        currentUrl,
+                        i.year
+                    )
+                )
+            }
+        }
+        return returnValue
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        // println("loadling link $data")
+
+    val manifests_url = "https://api.vanillo.tv/v1/_/watch/manifests?watchToken=${URLEncoder.encode(watchToken, "UTF-8")}"
+    val manifests_response = URL(manifests_url).readText()
+    val manifests_json = JSONObject(manifests_response)
+    val dash_url = manifests_json.getJSONObject("data").getJSONObject("media").getString("dash")
+    val hls_url = manifests_json.getJSONObject("data").getJSONObject("media").getString("hls")
+	
+            val mediaRootDocument = app.get(manifests_url).document
+			
+	        callback.invoke (
+            ExtractorLink(
+                name,
+                name,
+                data,
+                "",  // referer not needed
+                Qualities.Unknown.value,
+                false,
+                referrer="https://vanillo.tv",
+            )
+        )
+        return true
+		
+	}
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
+        val res = tryParseJson<Manifest>(app.get("https://api.vanillo.tv/v1/videos/recommended?limit=30").text) ?: return null
+        val lists = mutableListOf<HomePageList>()
+        res.data.videos.forEach {
+			    return provider.newMovieSearchResponse(name, thumbnail)
+        }
+        return HomePageResponse(
+            lists,
+            false
+        )
+    }
+    
+	/*
+    private data class CatalogResponse(val metas: List<CatalogEntry>)
+    private data class CatalogEntry(
+        val name: String,
+        val id: String,
+        val poster: String?,
+        val description: String?,
+        val type: String?,
+        val videos: List<Video>?
+*/
+
+}
